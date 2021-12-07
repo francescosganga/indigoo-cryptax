0f7e45e835d3448125982443f2465197981cdd8b
Francesco <sgangacreations@gmail.com>
2021-12-07 15:37:19 +0100

    wip

--- src/app/Controllers/MainController.php
+++ src/app/Controllers/MainController.php
@@ -15,8 +15,8 @@ class MainController
     const ACTION_PDF_MODELLO_REDDITI = 'pdf_modello_redditi';
     const ACTION_PDF_MODELLO_F24 = 'pdf_modello_f24';
 
-    public static function run() {
-        $reportWrapper = new ReportWrapper(file_get_contents(TRANSACTIONS_FILE));
+    public static function run($data) {
+        $reportWrapper = new ReportWrapper($data);
 
         $year = isset($_GET['year']) ? intval($_GET['year']) : DateUtils::getCurrentYear();
         $action = $_GET['action'] ?? null;
--- src/index.php
+++ src/index.php
@@ -8,20 +8,17 @@ use CrypTax\Controllers\WebAppController;
 use CrypTax\Exceptions\BaseException;
 use CrypTax\Exceptions\InvalidTransactionException;
 
-header('Access-Control-Allow-Origin: https://app.cryptax.xyz');
+header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: Content-Type, Cookies');
 header('Access-Control-Allow-Credentials: true');
 
 $data = json_decode(file_get_contents("php://input"), true);
-if (is_array($data)) {
-    $_POST = $_POST + $data;
-}
 
 set_time_limit(0);
 
 try {
     if (!defined('MODE') || MODE === 'private') {
-        MainController::run();
+        MainController::run($data);
     } else {
         WebAppController::run();
     }
